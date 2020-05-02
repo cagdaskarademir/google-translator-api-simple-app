6712ab9364b35b029b12da12237feffb2286147b
cagdaskarademir <cagdaskarademir@gmail.com>
2020-05-03 00:07:10 +0300

    First commit!

---
+++ src/GoogleTranslatorApi/GoogleTranslatorApi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Google.Cloud.Translation.V2;
+
+namespace GoogleTranslatorApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS","google-authentication-file.json");
+            TranslationClient client = TranslationClient.Create();
+
+            var result = client.TranslateText("Yurtta sulh, cihanda sulh", LanguageCodes.English, LanguageCodes.Turkish);
+
+            var translatedText = result.TranslatedText;
+
+            Console.WriteLine(translatedText);
+        }
+    }
+}
